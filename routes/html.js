2170950a9fa00a69e2fd68eab8bbf166e2d1412d
SimonM <smagidenko@gmail.com>
2023-08-24 20:14:32 -0400

    added routes for notes api and modularized code

--- routes/html.js
+++ routes/html.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
+const html = require('express').Router();
 const path = require('path');
 
+
 //serves up my index.html file
-router.get('/', (req, res) => {
+html.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 //serves up my notes.html file
-router.get('/notes', (req, res) => {
+html.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-module.exports = router
+module.exports = html
--- routes/notes.js
+++ routes/notes.js
@@ -1,10 +1,36 @@
-const router = require('express').Router();
+const notes = require('express').Router();
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+const dbFile = require('../db/db.json');
+
+notes.get('/notes', (req, res) => {
+    res.json(dbFile)
+});
+
+notes.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+    const randomID = Math.floor(Math.random() * 2400)
+
+    if (req.body) {
+        const newNote = {
+            id: randomID,
+            title,
+            text,
+        }
+        console.log(req.body);
+
+        dbFile.push(newNote);
+
+        readAndAppend(newNote, './db/db.json');
+
+        res.sendStatus(200)
+    } else {
+        res.error('Note failed to create')
+    }
+});
+// stringify dbFile and write it to File
 
 
 
 
 
-
-
-
-module.exports = router
+module.exports = notes
